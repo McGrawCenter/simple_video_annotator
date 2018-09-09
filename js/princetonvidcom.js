735affac9d8709cc7c607d47160bb964107e26cc
Ben Johnston <benj@princeton.edu>
2018-09-09 08:54:59 -0400

    first commit

---
+++ js/princetonvidcom.js
@@ -0,0 +1,141 @@
+
+
+jQuery( document ).ready(function() {
+
+
+
+
+var videos = $('video').mediaelementplayer({
+   success: function(media) {
+
+
+       var markers = [];
+
+  
+       function updateAnnotationList() {
+
+	    $.ajax({
+		url: "?a=json&post="+vars.post_id,
+		async: false, 
+		success: function(data){
+		  if(data[0] != null) { 
+
+		  jQuery("#annotation-list").empty();
+
+		  jQuery.each(data, function(i, v){
+		    var anno = decodeURIComponent(v.text);
+
+		    var html = "<div style='background:white;border:solid 1px grey;margin:10px 0px;padding:10px;border-radius:10px;'>\n";
+		    var html = html + "  <div style='font-size:0.8em;'>"+v.username+" "+v.date+"</div>\n";
+		    var html = html + "  <div><a href='#' class='seek' rel='"+v.start+"'><img src='"+vars.plugin_url+"images/play-button.png' style='float:left;margin:0px 10px 10px 0;'/></a> "
+		    var html = html + anno;
+		    var html = html + "  </div>\n";
+		    var html = html + "</div>\n";
+
+		    jQuery("#annotation-list").append(html);
+
+		    var percentage_from_zero =  (v.start / media.duration) * 100;
+		    var m = { 'id':v.id, 'start':v.start, 'pct':percentage_from_zero }
+		    markers.push(m);
+
+		  });
+		  } // end if not null
+
+	    	}
+	    });
+
+       }
+
+
+	// insert timeline and update with markers
+	function updateTimeline() {
+	   for(var x=0;x<=markers.length-1;x++) {
+	     jQuery("#puvidcom-timeline").append("<span style='position:absolute;left:"+markers[x].pct+"%;'><a href='#' class='seek' rel='"+markers[x].start+"'><span style='font-size:20px;' class='dashicons dashicons-arrow-down'></span></a></span>");
+	   }
+	}
+	jQuery(".mejs-video").after('<div id="annotation-list" style="background:black"></div>');
+	updateAnnotationList();
+	
+
+	// generate html for the toolbar
+	function toolbar() {
+	 var h = "<div id='puvidcom-toolbar' style='margin-top:10px;'>";
+	 h += "  <div id='puvidcom-toolbar-form'>";
+	 h += "  <input id='puvidcom-toolbar-start' type='text' name='start' style='' />";
+	 h += "  <textarea id='puvidcom-toolbar-text'></textarea>";
+	 h += "  <p style='margin-top:5px;'><button id='puvidcom-toolbar-add'>Annotate</button> <button>Um..</button> <button>Like..</button></p>";
+	 h += "  </div>";
+	 h += "</div>";
+	  return h;
+	}
+
+	var toolbar = toolbar();
+	jQuery(".mejs-video").after(toolbar);
+
+
+
+
+	// generate the timeline
+	function timeline() {
+	   var h = "<div id='puvidcom-timeline' style='height:28px;background:black; color:white;position:relative'>";
+           h += "</div>";
+	  return h;
+	}
+	jQuery(".mejs-video").after(timeline());
+	updateTimeline();
+
+
+      /** Press play button and jump to timecode **/
+
+	jQuery(document).on("click", ".seek" , function(e){
+	  var start = jQuery(this).attr('rel');
+	  media.setCurrentTime(start);
+	  media.play();
+	  e.preventDefault();
+	});
+
+      /** Add annotation **/
+
+	jQuery(document).on("click", "#puvidcom-toolbar-add" , function(){
+	    var postid = vars.post_id;
+	    var start = jQuery("#puvidcom-toolbar-start").val();
+	    var text = jQuery("#puvidcom-toolbar-text").val();
+	    if(text !='') {
+	      var payload = {'action':'annotatevideo','postid':postid,'start':start,'annotation':encodeURIComponent(text)};
+	      jQuery.post( "index.php", payload, function( data ) { updateAnnotationList();updateTimeline(); });
+	    }
+	});
+
+      /** If user clicks in the annotation textarea, pause the video and set the timecode **/
+
+	jQuery(document).on("click", "#puvidcom-toolbar-text" , function(){
+	    jQuery("#puvidcom-toolbar-start").val(media.getCurrentTime());
+	    jQuery("#puvidcom-toolbar-text").focus();  
+	    media.pause(); 
+	});
+
+
+
+
+      /** When user pauses, populate the start field with the start time **/
+
+       media.addEventListener('pause', function() {
+	    jQuery("#puvidcom-toolbar-start").val(media.getCurrentTime());
+	    jQuery("#puvidcom-toolbar-text").focus();          
+        }, true);
+
+
+
+
+
+    }
+});
+
+
+
+
+
+
+});
+
+
---
+++ videocommenter.php
@@ -0,0 +1,119 @@
+<?php 
+    /*
+    Plugin Name: Video Commenter
+    Plugin URI: http://www.princeton.edu
+    Description: Plugin for commenting on video embedded in Wordpress
+    Author: Ben Johnston - benj@princeton.edu
+    Version: 1.0
+    */
+
+
+
+
+/**************** REGISTER AND ENQUEUE SCRIPTS AND CSS ***************/
+
+function princetonvidcom_scripts()
+{
+  global $post;
+  wp_register_script('puvidcom', plugins_url('/js/princetonvidcom.js', __FILE__), array('jquery'),'1.1', true);
+  wp_enqueue_script('puvidcom');
+  $d = array('post_id'=> $post->ID, 'plugin_url' => plugin_dir_url( __FILE__ ));
+  wp_localize_script( 'puvidcom', 'vars', $d);
+
+}
+add_action( 'wp_enqueue_scripts', 'princetonvidcom_scripts' );
+
+
+
+/**************** PROCESS POSTED FORM SUBMISSION ***************/
+
+add_action( 'init', 'process_post' );
+
+
+function process_post() {
+     if( isset( $_POST['action'] )  && $_POST['action'] == 'annotatevideo' ) {
+	$user = wp_get_current_user();
+
+	$annotationObj = new StdClass();
+	$annotationObj->username = $user->user_login;
+	$annotationObj->userid = $user->ID;
+	$rand = mt_rand(100000,999999);
+	$annotationObj->id = $rand;
+	$annotationObj->date = date('m/d/Y h:i:s a', time());
+	$annotationObj->start = $_POST['start'];
+	$annotationObj->text = $_POST['annotation'];
+	add_post_meta($_POST['postid'], 'annotation', json_encode($annotationObj), false);
+
+        die(json_encode($annotationObj));
+     }
+}
+
+/**************** JSON ***************/
+add_action( 'init', 'princetonvidcom_json' );
+
+function princetonvidcom_json() {
+     if( isset( $_GET['a'] )  && $_GET['a'] == 'json' ) {
+	$postid = $_GET['post'];
+	$annotations = get_post_meta($postid, 'annotation');
+	$result = array();
+	foreach($annotations as $annotation) {
+	  $obj = json_decode($annotation);
+	  $result[] = $obj;
+	}
+	header('Content-Type: application/json');
+        die(json_encode($result));
+     }
+}
+
+
+
+
+/**************** EXCHANGE THE THEME'S COMMENT TEMPLATE FOR OUR OWN ***************/
+
+/*
+
+function princetonvidcom_comment_template( $comment_template ) {
+
+        return dirname(__FILE__) . "/comments.php";
+}
+
+add_filter( "comments_template", "princetonvidcom_comment_template" );
+*/
+
+
+/********************** SAVE META WHEN COMMENT SAVED ******************************/
+/*
+function add_ktimecode_meta( $comment_id ) {
+   if(isset($_POST['ktimecode']) && $_POST['ktimecode'] != 0 ) { 
+	add_comment_meta( $comment_id, 'ktimecode', $_POST['ktimecode'] );
+   }
+}
+
+add_action( 'comment_post', 'add_ktimecode_meta', 10, 2  );
+*/
+/********************** ADD TIMECODE FIELD TO COMMENT FORM ******************************/
+
+
+/*
+function change_comment_form_defaults( $default ) {
+    $commenter = wp_get_current_commenter();
+    $default[ 'comment_field' ] .= '<input type="text" id="ktimecode" name="ktimecode"/>';
+    return $default;
+}
+
+add_filter( 'comment_form_defaults', 'change_comment_form_defaults');
+
+
+function puvc_alter_comment ($comment, $args, $depth) {
+  //$GLOBALS['comment'] = $comment;
+
+  if($meta = get_comment_meta( $comment->comment_ID, 'ktimecode', true )) {
+    echo "<div>This is a comment with a timecode and the timecode is: <a href='?ktimecode={$meta}'>{$meta}</a></div>";
+  }
+  else {
+    echo "<div>This is a comment without a timecode</div>";
+  }
+}
+*/
+
+?>
